6d2e06d183d0471eac64d6024bfbeb10bb0c2666
sampremm <samprem888111@gmail.com>
2024-12-04 09:59:11 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,8 +5,7 @@ import { motion } from "framer-motion";
 const containerVariants = {
   hidden: { opacity: 0, y:-20 },
   visible: {
-    opacity: 1,
-    y: 0,
+    opacity: 1,y: 0,
     transition: { staggerChildren: 0.4, duration: 0.5 },
   },
 };
@@ -14,8 +13,7 @@ const containerVariants = {
 const itemsVariants = {
   hidden: { opacity: 0, x:0.8  },
   visible: {
-    opacity: 1, scale: 1  , transition: { duration: 0.5 }, 
-    transition: { duration: 0.5 },
+    opacity: 1, scale: 1  , transition: { duration: 0.5 },
   },
 };
 
